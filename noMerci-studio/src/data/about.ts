d0f9cddf78b10a7a707005c341cfe0cafbc78397
Bxdda (Chaughn) <123420597+bxdda@users.noreply.github.com>
2026-04-19 08:10:43 -0400

    0.2.2
    - about on navbar fixed it seems
    - no browser tab title change on homepage, working

--- noMerci-studio/src/data/about.ts
+++ noMerci-studio/src/data/about.ts
@@ -5,8 +5,8 @@ export const aboutData = {
     {
       heading: "Overview",
       text: [
-        "We are a U.S.-based game studio focused on building high-performance, interactive experiences using Java. Our work is powered by LibGDX for cross-platform development and Spine for fluid, expressive animation",
-        "We specialize in blending 2D and 3D systems to create unique visual styles and responsive gameplay. From browser-based releases to desktop experiences, our goal is to deliver games that feel fast, polished, and intentional",
+        "We are a U.S.-based game studio focused on building high-performance, interactive experiences using Java. Our work is powered by LibGDX for cross-platform development and Spine for fluid, expressive animation.",
+        "We specialize in blending 2D and 3D systems to create unique visual styles and responsive gameplay. From browser-based releases to desktop experiences, our goal is to deliver games that feel fast, polished, and intentional.",
       ],
     },
     {
